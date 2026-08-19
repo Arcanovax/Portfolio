87bf9f6985d1443d9f4fe910b7b7821fbab7330f
Arcanovax <t.matisse2007@gmail.com>
2026-08-20 00:35:50 +0200

    timeline

--- src/portfolio/Portfolio.tsx
+++ src/portfolio/Portfolio.tsx
@@ -60,7 +60,7 @@ export default function Portfolio() {
 		<div className="relative min-h-screen overflow-hidden bg-[#1C1F24] text-foreground">
 
 		
-			<div className="mx-auto max-w-150 my-20 border text-white">
+			<div className="mx-auto max-w-150 my-10 border text-white">
 				<div className="p-3 h-48">
 					<div className='flex flex-row gap-3'>
 						<img src={pfp} alt="Hero" className='h-25'/>
@@ -79,10 +79,10 @@ export default function Portfolio() {
 					</div>
 					 
 				</div>
-				{/* <div className="bg-red-700 h-100">
+				<div className="pt-9 h-64">
 					Hi, I'm Matisse, a 42 Student School passionnate about coding
-				</div> */}
-				<div className=" h-50">
+				</div>
+				<div className="h-50">
 					<div className='flex flex-row'>					
 						<div className=" m-5 grid grid-cols-4 grid-rows-3 gap-2 w-60">
 							{svgData.map((item) => (
@@ -102,6 +102,74 @@ export default function Portfolio() {
 							
 						</div>
 					</div>
+				</div>
+				<div className="h-250">
+					<div className="p-8">
+						<h2 className="text-3xl text-white font-bold mb-5">Chronology</h2>
+						<div className="flex flex-col grid-cols-9 p-2 mx-auto md:grid">
+							<div className="flex md:contents flex-row-reverse">
+								<div
+									className="relative p-4 my-6 text-gray-800 bg-white rounded-xl col-start-1 col-end-5 mr-auto md:mr-0 md:ml-auto">
+									<h3 className="text-lg font-semibold lg:text-xl">New Event 1</h3>
+									<p className="mt-2 leading-6">Description of the first event.</p>
+									<span className="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">2020</span>
+								</div>
+								<div className="relative col-start-5 col-end-6 mr-7 md:mx-auto">
+									<div className="flex items-center justify-center w-6 h-full">
+										<div className="w-1 mt-22 h-1/2 bg-white">
+										</div>
+									</div>
+									<div className="absolute w-6 h-6 -mt-3 bg-white rounded-full top-1/2"></div>
+								</div>
+							</div>
+
+							<div className="flex md:contents">
+								<div className="relative col-start-5 col-end-6 mr-7 md:mx-auto">
+									<div className="flex items-center justify-center w-6 h-full">
+										<div className="w-1 h-full bg-white"></div>
+									</div>
+									<div className="absolute w-6 h-6 -mt-3 bg-white  rounded-full top-1/2"></div>
+								</div>
+								<div className="relative p-4 my-6 text-gray-800 bg-white rounded-xl col-start-6 col-end-10 mr-auto">
+									<h3 className="text-lg font-semibold lg:text-xl">New Event 2</h3>
+									<p className="mt-2 leading-6">Description of the second event.</p>
+									<span className="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">Date 2</span>
+								</div>
+							</div>
+
+							<div className="flex md:contents flex-row-reverse">
+								<div
+									className="relative p-4 my-6 text-gray-800 bg-white rounded-xl col-start-1 col-end-5 mr-auto md:mr-0 md:ml-auto">
+									<h3 className="text-lg font-semibold lg:text-xl">New Event 3</h3>
+									<p className="mt-2 leading-6">Description of the first event.</p>
+									<span className="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">Date 3</span>
+								</div>
+								<div className="relative col-start-5 col-end-6 mr-7 md:mx-auto">
+									<div className="flex items-center justify-center w-6 h-full">
+										<div className="w-1 h-full bg-white">
+										</div>
+									</div>
+									<div className="absolute w-6 h-6 -mt-3 bg-white  rounded-full top-1/2"></div>
+								</div>
+							</div>
+
+							<div className="flex md:contents">
+								<div className="relative col-start-5 col-end-6 mr-7 md:mx-auto">
+									<div className="flex items-center justify-center w-6 h-full">
+										<div className="w-1 h-full bg-white"></div>
+									</div>
+									<div className="absolute w-6 h-6 -mt-3 bg-white border-4 rounded-full top-1/2"></div>
+								</div>
+								<div className="relative p-4 my-6 text-gray-800 bg-white rounded-xl col-start-6 col-end-10 mr-auto">
+									<h3 className="text-lg font-semibold lg:text-xl">New Event 4</h3>
+									<p className="mt-2 leading-6">Description of the second event.</p>
+									<span className="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">Date 4</span>
+								</div>
+							</div>
+
+						</div>
+					</div>
+
 				</div>
 
 			</div>
